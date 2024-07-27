6478efc0ad75876d276e3d1a5ef79b1b1a3ea413
ImAadarsh <aadarshkavita@gmail.com>
2024-07-27 17:12:48 +0530

    AA

--- include/meta.php
+++ include/meta.php
@@ -2,8 +2,8 @@
 <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="author" content="gramentheme">
-        <meta name="description" content="Kidsa - Kindergarten School HTML Template">
+        <meta name="author" content="Endeavour Digital">
+        <meta name="description" content="Endeavour Digital Development Project">
 <!--<< Favcion >>-->
 <link rel="shortcut icon" href="assets/img/logo/logo.svg">
         <!--<< Bootstrap min.css >>-->
